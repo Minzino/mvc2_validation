0d999a4c7689d822720b1f030f12eeb8162512fd
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-08-15 02:40:30 +0900

    Feat: validator 분리

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -24,6 +24,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Slf4j
 public class ValidationItemControllerV2 {
 
+	private final ItemValidator itemValidator;
 	private final ItemRepository itemRepository;
 
 	@GetMapping
@@ -166,7 +167,7 @@ public class ValidationItemControllerV2 {
 		return "redirect:/validation/v2/items/{itemId}";
 	}
 
-	@PostMapping("/add")
+//	@PostMapping("/add")
 	public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult,
 		RedirectAttributes redirectAttributes,
 		Model model) {
@@ -190,6 +191,25 @@ public class ValidationItemControllerV2 {
 				bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
 			}
 		}
+		//검증에 실패하면 다시 입력 폼으로
+		if (bindingResult.hasErrors()) {
+			log.info("errors={}", bindingResult);
+			return "validation/v2/addForm";
+		}
+
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+
+	@PostMapping("/add")
+	public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
+		RedirectAttributes redirectAttributes,
+		Model model) {
+
+		itemValidator.validate(item, bindingResult);
+
 		//검증에 실패하면 다시 입력 폼으로
 		if (bindingResult.hasErrors()) {
 			log.info("errors={}", bindingResult);
